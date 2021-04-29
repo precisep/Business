c62cb564f484ef50b876607c8a274c9d3b43ea8f
Thabang Mokoena <79963271+precisep@users.noreply.github.com>
2021-04-29 18:48:44 +0200

    Important Files
    Settings

---
+++ website/urls.py
@@ -0,0 +1,16 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, include
+from affiliate import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.contrib.staticfiles.urls import static
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('',include('affiliate.urls')),
+]
+if settings.DEBUG:
+    urlpatterns += staticfiles_urlpatterns()
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
